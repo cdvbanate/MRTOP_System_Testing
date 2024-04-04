587e040f863ade88cf641c81da5d56a06cc7b064
cristianlegaspi <cglegaspi@tesda.gov.ph>
2024-04-04 17:37:03 +0800

    Latest Commit

--- resources/views/customFooter.blade.php
+++ resources/views/customFooter.blade.php
@@ -1,5 +1,5 @@
-<footer class="fixed bottom-0 left-0 z-20 w-full p-2 sm:p-3 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-4 md:py-2 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-xs sm:text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024
-        <a href="#" class="text-xs sm:text-sm hover:underline">MRTOP Onboarding System </a>| Developed By : eTESDA Division IT Technical Team
+<footer class="fixed bottom-0 left-0 z-20 w-full p-1 sm:p-2 bg-white border-t border-gray-200 shadow md:flex md:items-center md:p-3 md:py-1 dark:bg-gray-800 dark:border-gray-600">
+    <span style="font-size: 0.7rem; margin: auto;" class="text-gray-400 sm:text-xs text-center dark:text-gray-300 block">© 2024
+        <a href="#" style="font-size: 0.7rem;" class="sm:text-xs hover:underline">MRTOP Onboarding System </a>| Developed By : eTESDA Division IT Technical Team
     </span>
 </footer>
