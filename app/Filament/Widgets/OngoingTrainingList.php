d802c5f2299d1f350cc5d99225a650559058bbe6
cristianlegaspi <cglegaspi@tesda.gov.ph>
2024-06-27 15:28:58 +0800

    Ongoing Training Update

---
+++ app/Filament/Widgets/OngoingTrainingList.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Tables;
+use App\Models\Request;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Auth;
+use Filament\Resources\Components\Tab;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class OngoingTrainingList extends BaseWidget
+{
+    protected static ?int $sort = 4;
+
+    protected int | string | array $columnSpan ='full';
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Request::query())
+            ->query(Request::query()->where('TrainingStatus', 'Ongoing')) 
+            ->defaultSort('created_at', 'desc')
+            ->modifyQueryUsing(function (Builder $query) {
+                $user = Auth::user();
+                if ($user->role === 'ADMIN') {
+                    return;
+                }
+                $userId = $user->id;
+                $query->where('user_id', $userId);
+            })
+            ->columns([
+
+                Tables\Columns\TextColumn::make('qualification.qualification_name'),
+                Tables\Columns\TextColumn::make('NameOfTrainer')
+                ->label('Trainer'),
+                Tables\Columns\TextColumn::make('targetStart'),
+                Tables\Columns\TextColumn::make('targetEnd'),
+                Tables\Columns\TextColumn::make('TrainingStatus')
+                ->label('Training Status')
+                ->badge()
+                ->color(fn (string $state): string => match ($state) {
+                    // 'Not Yet Started' => 'danger',
+                    'Ongoing' => 'warning',
+                    // 'Completed' => 'success',
+                }),
+                Tables\Columns\TextColumn::make('created_at')
+                ->label('Date Requested')
+                ->sortable(), 
+               
+            ])
+            ->filters([
+                // SelectFilter::make('TrainingStatus')
+                //     ->label('Filter By Training Status')
+                //     ->preload()
+                //     ->options([
+                //         'Not Yet Started' => 'Not Yet Started',
+                //         'Ongoing' => 'Ongoing',
+                //         'Completed' => 'Completed',
+                //     ]),
+            ]);
+            
+       
+        
+    }
+    public static function canView(): bool
+    {
+        return auth()->user()->isAdmin();
+    }
+}
