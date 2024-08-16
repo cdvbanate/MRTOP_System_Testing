82e652f90b726379e6761187263dfa112c210f15
cristianlegaspi <cglegaspi@tesda.gov.ph>
2024-08-16 11:37:06 +0800

    DownloadableFiles

---
+++ app/Filament/Resources/DownloadableResource/Pages/EditDownloadable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\DownloadableResource\Pages;
+
+use App\Filament\Resources\DownloadableResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDownloadable extends EditRecord
+{
+    protected static string $resource = DownloadableResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
