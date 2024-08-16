a95d9e968ed08666639ef12a776c76a55f02f979
cristianlegaspi <cglegaspi@tesda.gov.ph>
2024-08-16 17:42:37 +0800

    latest commit

--- app/Filament/Resources/DownloadResource.php
+++ app/Filament/Resources/DownloadResource.php
@@ -2,16 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\DownloadResource\Pages;
-use App\Filament\Resources\DownloadResource\RelationManagers;
-use App\Models\Download;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Download;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use App\Filament\Resources\DownloadResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\DownloadResource\RelationManagers;
 
 class DownloadResource extends Resource
 {
@@ -19,11 +22,25 @@ class DownloadResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $modelLabel= 'Downloadable Files';
+    protected static ?int $navigationSort = 4;
+
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+
+                Forms\Components\Section::make('Downloadable File Information')
+                ->description('Please provide complete data here.')
+                ->schema([  
+                Forms\Components\TextInput::make('Download')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('LinkToDownload')
+                    ->required()
+                    ->maxLength(255),
+                    ])
             ]);
     }
 
@@ -31,7 +48,18 @@ class DownloadResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('Download')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('LinkToDownload')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
@@ -54,12 +82,28 @@ class DownloadResource extends Resource
         ];
     }
 
+    public static function Infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+            Section::make('Downloadable File Information')
+              ->schema([
+                TextEntry::make('DocumentName')->label('Document Name'),
+                TextEntry::make('Link')->label('Downloadble Link')
+                ->copyable()
+                ->copyMessage('Copied!')
+                ->copyMessageDuration(1500),            
+              ])
+            ]);
+    }
+
+
     public static function getPages(): array
     {
         return [
             'index' => Pages\ListDownloads::route('/'),
             'create' => Pages\CreateDownload::route('/create'),
-            'view' => Pages\ViewDownload::route('/{record}'),
+            // 'view' => Pages\ViewDownload::route('/{record}'),
             'edit' => Pages\EditDownload::route('/{record}/edit'),
         ];
     }
